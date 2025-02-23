e0c02ae065cf4f87b37e875c119aef925c218c94
MedEzzedineBenZbiba <mohamedezzedine.benzbiba@esprit.tn>
2025-02-23 11:23:58 +0100

    Split monolithic app into microservices, add API gateway and Eureka Server
    Divided the monolithic application into multiple microservices, each with its appropriate database (SGBD).
    Implemented an API Gateway to manage traffic between microservices.
    Added and configured Eureka Server for service discovery, centralizing microservice registration.

---
+++ GatewayProject/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,30 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("EDUDIANT", r->r.path("/etudiant/**").uri("lb://EDUDIANT"))
+                .route("RESERVATION", r->r.path("/reservation/**").uri("lb://RESERVATION"))
+                .route("UNIVERSITY", r->r.path("/university/**").uri("lb://UNIVERSITY"))
+                .route("BLOCCHAMBRE", r->r.path("/bloc/**").uri("lb://BLOCCHAMBRE"))
+                .route("BLOCCHAMBRE", r->r.path("/chambre/**").uri("lb://BLOCCHAMBRE"))
+                .route("FOYER", r->r.path("/foyer/**").uri("lb://FOYER"))
+                .build();
+    }
+
+}
