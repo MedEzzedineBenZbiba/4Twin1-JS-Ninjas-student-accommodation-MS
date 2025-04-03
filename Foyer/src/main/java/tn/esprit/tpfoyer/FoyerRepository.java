0bc0dde1d13fe5ba44398226d6235236b1f9bb5f
MedEzzedineBenZbiba <mohamedezzedine.benzbiba@esprit.tn>
2025-04-03 18:05:36 +0100

    Fix database connection problem

--- Foyer/src/main/java/tn/esprit/tpfoyer/FoyerRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.tpfoyer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.tpfoyer.Foyer;
-
-@Repository
-public interface FoyerRepository extends JpaRepository<Foyer, Long> {
-}
