0d22734b49ef8bd0f846859f562f3c1e2ca0d8ff
taher mabrouk <Mabrouk.taher@esprit.tn>
2025-04-24 14:19:37 +0100

    communication with frontend+

--- services/gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
@@ -18,54 +18,6 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-//	@Bean
-//	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//		return builder.routes()
-//				.route("etudiant-service", r -> r.path("/etudiant/**")
-//						.uri("lb://etudiant-service"))
-//				.route("reservation-service", r -> r.path("/reservation/**")
-//						.uri("lb://reservation-service"))
-//				.route("university-service", r -> r.path("/university/**")
-//						.uri("lb://university-service"))
-//				.route("bloc-service", r -> r.path("/bloc/**")
-//						.uri("lb://BlocChambre-service"))
-//				.route("chambre-service", r -> r.path("/chambre/**")
-//						.uri("lb://BlocChambre-service"))
-//				.route("foyer-service", r -> r.path("/foyer/**")
-//						.uri("lb://foyer-service"))
-//				.route("mailing-service", r -> r.path("/mailing/**")
-//						.filters(f -> f.stripPrefix(1))
-//						.uri("http://localhost:4000"))
-//				.build();
-//	}
-
-	/*@Bean
-	public CorsWebFilter corsFilter() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://localhost:5173");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-
-		return new CorsWebFilter(source);
-	}*/
-//	@Bean
-//	public CorsWebFilter corsFilter() {
-//		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.setAllowedOriginPatterns(List.of("http://localhost:5173")); // Important !
-//		config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
-//		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-//		config.setMaxAge(3600L);
-//
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", config);
-//
-//		return new CorsWebFilter(source);
-//	}
 	@Bean
 	public CorsWebFilter corsFilter() {
 		CorsConfiguration config = new CorsConfiguration();
